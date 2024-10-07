0c3d2fbc3b2c75efad31c28ccefdf7ee77085421
Ralph Segi <segiralph@gmail.com>
2024-10-07 15:47:19 -0700

    Make Dynamic Dialogs full screen by default on screens smaller than 768px

--- frontend/config/dialog-props.ts
+++ frontend/config/dialog-props.ts
@@ -5,13 +5,18 @@ export const ModalConfig = {
     closeOnEscape: true,
     blockScroll: true,
     keepInViewPort: true,
-    maximizable: true,
+    maximizable: false,
     style: {
         width: '50vw',
     },
     breakpoints:{
         '960px': '75vw',
         '640px': '96vw'
+    },
+    pt: {
+        root: {
+            class: "max-dialog-maximized-mobile",
+        },
     },
     modal: true,
     draggable: false,
