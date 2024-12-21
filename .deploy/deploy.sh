2a06051e00d919a9977e4bd3e2738d1c6d731aac
Ralph Segi <segiralph@gmail.com>
2024-12-21 13:05:17 -0600

    Rework Github actions to use official runners

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+set -x
 
-SUCCEEDED=$(curl -X GET "${DEPLOY_URL}" -H "X-Access-Token: ${DEPLOY_TOKEN}" | grep "Done" | wc -l)
+SUCCEEDED=$(curl -X GET "${DEPLOY_URL}" -H "X-Access-Token: ${DEPLOY_TOKEN}" | grep "OK" | wc -l)
 
 if [ "${SUCCEEDED}" != 1 ] ; then
   echo "Deployment failed!"
