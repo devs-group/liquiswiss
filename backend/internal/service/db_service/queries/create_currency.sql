56276c1933950fa74aa7657a8098ae1108e3278b
Ralph Segi <segiralph@gmail.com>
2025-07-09 12:06:34 -0500

    Adjust create currency SQL for better test compatibility

--- backend/internal/service/db_service/queries/create_currency.sql
+++ backend/internal/service/db_service/queries/create_currency.sql
@@ -1,2 +1,5 @@
 INSERT INTO currencies (code, description, locale_code)
 VALUES (?, ?, ?)
+ON DUPLICATE KEY UPDATE description = VALUES(description),
+                        locale_code = VALUES(locale_code),
+                        code        = VALUES(code);
